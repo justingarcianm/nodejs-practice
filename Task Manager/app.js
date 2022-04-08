5ae85409332cae52435b504cbed75ca41373dc1b
justingarcianm <justingarcianm@gmail.com>
2022-04-08 13:26:33 -0600

    basics of error handling

--- Task Manager/app.js
+++ Task Manager/app.js
@@ -3,6 +3,9 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./DB/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
 // middleware
 app.use(express.static('./public'))
 app.use(express.json())
@@ -10,14 +13,10 @@ app.use(express.json())
 // routes
 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-// app.get('/api/v1/tasks) - get all tasks
-// app.post('/api/v1/tasks) - create new task
-// app.get('/api/v1/tasks/:id) - get single task
-// app.patch('/api/v1/tasks/:id) - update task
-// app.delete('/api/v1/tasks/:id) - delete task
-
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
---
+++ Task Manager/middleware/error-handler.js
@@ -0,0 +1,11 @@
+const {CustomAPIError} = require('../errors/custom-error')
+
+const errorHandlerMiddleware = (err,req,res,next) => {
+    console.log(err)
+    if(err instanceof CustomAPIError) {
+        return res.status(err.statusCode).json({msg:err.message})
+    }
+    return res.status(400).json({msg:'Something went wrong...'})
+}
+
+module.exports = errorHandlerMiddleware
